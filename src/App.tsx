a6ca0aba83a50f54ef6dca154358bf3fb18ddc69
LorenaBarr <karenlbarrera@gmail.com>
2025-02-25 18:43:59 -0500

    todolist localstore

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TaskList from "./components/TaskList";
 import TaskForm from "./components/TaskForm";
 import "./index.css"; 
 
+const STORAGE_KEY = "tasks";
+
 const App: React.FC = () => {
-  const [tasks, setTasks] = useState<{ id: number; text: string; completed: boolean }[]>([]);
+  const [tasks, setTasks] = useState<{ id: number; text: string; completed: boolean }[]>(() => {
+    const savedTasks = localStorage.getItem(STORAGE_KEY);
+    return savedTasks ? JSON.parse(savedTasks) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks));
+  }, [tasks]);
 
   const addTask = (text: string) => {
     setTasks([...tasks, { id: Date.now(), text, completed: false }]);
@@ -30,3 +39,4 @@ const App: React.FC = () => {
 };
 
 export default App;
+
